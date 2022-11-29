95eeea86e6e3ad29f378dde00f5eec25188272e4
Jason Hinchcliffe <hinchy@gmail.com>
2022-11-29 16:58:17 +1100

    add some timewasters...

--- script.sh
+++ script.sh
@@ -1,11 +1,57 @@
 #!/bin/bash
 set -eo pipefail
 
-echo "starting...."
+echo "starting....🎤"
 
-sleep 10
+sleep 1
 
-echo "finishing"
+for i in {1..10}; do
+  echo i
+  echo ""
+  echo "Yesterday"
+  sleep 10
+  echo "All my troubles seemed so far away"
+  sleep 10
+  echo "Now it looks as though they're here to stay"
+  sleep 10
+  echo "Oh, I believe in yesterday"
+  sleep 10
+  echo "[Verse 2]"
+  sleep 10
+  echo "Suddenly"
+  sleep 10
+  echo "I'm not half the man I used to be"
+  sleep 10
+  echo "There's a shadow hanging over me"
+  sleep 10
+  echo "Oh, yesterday came suddenly"
+  sleep 10
+  echo "[Chorus]"
+  sleep 10
+  echo "Why she had to go? I don't know, she wouldn't say"
+  sleep 10
+  echo "I said something wrong, now I long for yesterday"
+  sleep 10
+  echo "[Verse 3]"
+  sleep 10
+  echo "Yesterday"
+  sleep 10
+  echo "Love was such an easy game to play"
+  sleep 10
+  echo "Now I need a place to hide away"
+  sleep 10
+  echo "Oh, I believe in yesterday"
+  sleep 10
+  echo "[Chorus]"
+  sleep 10
+  echo "Why she had to go? I don't know, she wouldn't say"
+  sleep 10
+  echo "I said something wrong, now I long for yesterday"
+  sleep 10
+  echo "..."
+done
+
+echo "finishing 😅"
 
 sleep 1
 
